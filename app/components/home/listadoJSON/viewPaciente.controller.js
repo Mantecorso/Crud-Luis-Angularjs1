24eefa9b478e671d8598d14ad3048a7892db9f84
Mantecorso <luisjuradoquesada@gmail.com>
2018-08-13 11:02:21 +0200

    new view controllers

---
+++ app/components/home/listadoJSON/viewPaciente.controller.js
@@ -0,0 +1,20 @@
+class ViewPacienteController{   
+    constructor(datosService, $location) {
+        let path = $location.$$path.split('/')
+        
+        this.datosService = datosService;
+        this.getID(path[path.length -1]);
+        
+    }
+
+    getID(id){
+        this.datosService.getData()
+            .then(resultado =>{
+                 
+                this.user = resultado.filter(item => item.id == + id)[0];
+            })
+    }
+   
+}
+
+export default ViewPacienteController;
