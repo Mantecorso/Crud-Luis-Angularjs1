80cc4bd1385eba18de0baaea8276bb013b09b127
Mantecorso <luisjuradoquesada@gmail.com>
2018-08-17 12:33:12 +0200

    inicio responsive

--- app/common/header/header.component.js
+++ app/common/header/header.component.js
@@ -56,6 +56,7 @@ export const HeaderComponent = {
         </div>
         
     </header>
+    
     `
 }
 
--- app/components/home/inicio/home.component.js
+++ app/components/home/inicio/home.component.js
@@ -6,8 +6,8 @@ export const HomeComponent = {
     },
     controller,
     template: `
-    <div class="container-fluid" id="cuerpo">
-    
+    <div class="container-fluid d-none d-md-block" id="cuerpo">
+   
         <div class="row">
         <div class="col-2"></div>
             <div class=" col-8 espace table-responsive-sm">
@@ -69,6 +69,67 @@ export const HomeComponent = {
                 </table>
             </div>
             <div class="col-2"></div>
+        </div>
+    </div>
+    <div class="container-fluid d-block d-md-none" id="cuerpo">
+    
+        <div class="row">
+        
+            <div class="col-12 espace table-responsive-sm">
+                <table class="table table-hover"
+                    <thead class="thead-dark">
+                        <tr>                    
+                            <th><h4> Nombre </h4></th>
+                            <th><h4> Editar </h4></th>
+                            <th><h4> Ver </h4></th>
+                            <th><h4> Borrar </h4></th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr ng-repeat="user in $ctrl.dataResult track by user.id">
+                                     
+                            <td>{{user.nombre}}</td>
+                            <td>
+                                <div ng-if="user.usuario === 'paciente'"><a ui-sref="editPaciente({userID:{{ user.id}}})" class="btn btn-dark nerea"> Edit</a></div>
+                                <div ng-if="user.usuario === 'profesional'"><a ui-sref="editProf({userID:{{ user.id}}})" class="btn btn-dark nerea">Edit</a></div>                                </td>
+                            <td>
+                                <div ng-if="user.usuario === 'paciente'"><a ui-sref="viewPaciente({userID:{{user.id}}})" class="btn btn-dark nerea">View</a></div>
+                                <div ng-if="user.usuario === 'profesional'"><a ui-sref="viewProf({userID:{{user.id}}})" class="btn btn-dark nerea">View</a></div>
+                            </td>            
+                            <td>                                           
+                                <a data-toggle="modal" data-target="#deleteUser{{user.id}}" href="#" class="btn btn-dark nerea">Delete</a>
+                                <div class="modal " id="deleteUser{{user.id}}" tabindex="-1" role="dialog" aria-labelledby="EliminarUsers" aria-hidden="true">
+                                    <div class="modal-dialog modal-dialog-centered" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title">
+                                                    Borrar Usuario
+                                                </h5>
+                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                    <span aria-hidden="true">&times;</span>
+                                                </button>
+                                            </div>
+                                            <div class="modal-body">
+                                                Advertencia, vas a borrar a este usuario ¿Estás seguro?
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="submit" ng-click="$ctrl.deleteUser(user.id)" class="btn btn-secondary">
+                                                    Si
+                                                </button>
+                                                <button type="button" class="btn btn-primary" data-dismiss="modal">
+                                                    No
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </td>            
+                        </tr>
+                                  
+                    </tbody>
+                </table>
+            </div>
+            
         </div>
     </div>
     `
